6028a76ad38d9e595e471999791ce840553cddba
sissi <whmobil@gmail.com>
2018-10-28 09:12:14 +0100

    Update Espalexa.h
    Better integration in Projekts. Change Name of running devices.
    Multiple Device can be handelt with one callback.

--- src/Espalexa.h
+++ src/Espalexa.h
@@ -1,4 +1,4 @@
-﻿#ifndef Espalexa_h
+#ifndef Espalexa_h
 #define Espalexa_h
 
 #include "Arduino.h"
@@ -10,57 +10,135 @@
 #include <ESP8266WebServer.h>
 #endif
 
-typedef void (*CallbackBriFunction) (uint8_t br);
+typedef void (*CallbackBriFunction)(uint8_t br, uint8_t last_br, String name, int index);
 
-class EspalexaDevice {
+class EspalexaDevice
+{
 private:
-		String _deviceName;
-		CallbackBriFunction _callback;
-		uint8_t _val, _val_last;
+	String _deviceName;
+	CallbackBriFunction _callback;
+	uint8_t _val, _val_last;
+
 public:
-		EspalexaDevice();
-		~EspalexaDevice();
-		EspalexaDevice(String deviceName, CallbackBriFunction gnCallback, uint8_t initialValue =0);
-		
-		String getName();
-		uint8_t getValue();
-		
-		void setValue(uint8_t bri);
-		
-		void doCallback();
-		
-		uint8_t getLastValue(); //last value that was not off (1-255)
+	EspalexaDevice();
+	~EspalexaDevice();
+	EspalexaDevice(String deviceName, CallbackBriFunction gnCallback, uint8_t initialValue = 0);
+
+	String getName();
+	void setName(String deviceName);
+	uint8_t getValue();
+
+	void setValue(uint8_t bri);
+
+	void doCallback(int index);
+
+	uint8_t getLastValue(); //last value that was not off (1-255)
 };
 
-class Espalexa {
+class Espalexa
+{
 private:
-		void startHttpServer();
-		String deviceJsonString(uint8_t deviceId);
-        void handleDescriptionXml();
-		void respondToSearch();
-		String boolString(bool st);
-		bool connectUDP();
-		void alexaOn(uint8_t deviceId);
-		void alexaOff(uint8_t deviceId);
-		void alexaDim(uint8_t deviceId, uint8_t briL);
+	void startHttpServer();
+	String deviceJsonString(uint8_t deviceId);
+	void handleDescriptionXml();
+	void respondToSearch();
+	String boolString(bool st);
+	bool connectUDP();
+	void alexaOn(uint8_t deviceId);
+	void alexaOff(uint8_t deviceId);
+	void alexaDim(uint8_t deviceId, uint8_t briL);
+
 public:
-		Espalexa();
-		~Espalexa();
-		#ifdef ARDUINO_ARCH_ESP32
-		WebServer* server;
-		bool begin(WebServer* externalServer=nullptr);
-		#else
-		ESP8266WebServer* server;
-	    bool begin(ESP8266WebServer* externalServer=nullptr);
-		#endif
+	Espalexa();
+	~Espalexa();
+#ifdef ARDUINO_ARCH_ESP32
+	WebServer *server;
+	bool begin(WebServer *externalServer = nullptr);
+#else
+	ESP8266WebServer *server;
+	bool begin(ESP8266WebServer *externalServer = nullptr);
+#endif
 
-		bool addDevice(EspalexaDevice* d);
-		bool addDevice(String deviceName, CallbackBriFunction callback, uint8_t initialValue=0);
+	bool addDevice(EspalexaDevice *d);
+	bool addDevice(String deviceName, CallbackBriFunction callback, uint8_t initialValue = 0);
+	EspalexaDevice *getDevice(int pos);
+	void removeAllDevices();
+	int getDeviceCount();
+	void loop();
 
-		void loop();
-		
-		bool handleAlexaApiCall(String req, String body);
+	bool handleAlexaApiCall(String req, String body);
 };
 
 #endif
+#ifndef Espalexa_h
+#define Espalexa_h
 
+#include "Arduino.h"
+//#include <WiFiUDP.h>
+
+#ifdef ARDUINO_ARCH_ESP32
+#include "dependencies/webserver/WebServer.h"
+#else
+#include <ESP8266WebServer.h>
+#endif
+
+typedef void (*CallbackBriFunction)(uint8_t br, uint8_t last_br, String name, int index);
+
+class EspalexaDevice
+{
+private:
+	String _deviceName;
+	CallbackBriFunction _callback;
+	uint8_t _val, _val_last;
+
+public:
+	EspalexaDevice();
+	~EspalexaDevice();
+	EspalexaDevice(String deviceName, CallbackBriFunction gnCallback, uint8_t initialValue = 0);
+
+	String getName();
+	void setName(String deviceName);
+	uint8_t getValue();
+
+	void setValue(uint8_t bri);
+
+	void doCallback(int index);
+
+	uint8_t getLastValue(); //last value that was not off (1-255)
+};
+
+class Espalexa
+{
+private:
+	void startHttpServer();
+	String deviceJsonString(uint8_t deviceId);
+	void handleDescriptionXml();
+	void respondToSearch();
+	String boolString(bool st);
+	bool connectUDP();
+	void alexaOn(uint8_t deviceId);
+	void alexaOff(uint8_t deviceId);
+	void alexaDim(uint8_t deviceId, uint8_t briL);
+
+public:
+	Espalexa();
+	~Espalexa();
+#ifdef ARDUINO_ARCH_ESP32
+	WebServer *server;
+	bool begin(WebServer *externalServer = nullptr);
+#else
+	ESP8266WebServer *server;
+	bool begin(ESP8266WebServer *externalServer = nullptr);
+#endif
+
+	bool addDevice(EspalexaDevice *d);
+	bool addDevice(String deviceName, CallbackBriFunction callback, uint8_t initialValue = 0);
+	EspalexaDevice *getDevice(int pos);
+	void removeAllDevices();
+	int getDeviceCount();
+	void loop();
+
+	bool handleAlexaApiCall(String req, String body);
+};
+
+#endif
