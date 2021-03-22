f622349c963a4b108f7ad839a7c1526762db62f2
oponyx <78806035+oponyx@users.noreply.github.com>
2021-03-22 01:09:50 +0100

    Better uniqueid encoding

--- src/Espalexa.h
+++ src/Espalexa.h
@@ -123,7 +123,7 @@ private:
     uint8_t mac[6];
     WiFi.macAddress(mac);
 
-    sprintf(out, "%2X:%2X:%2X:%2X:%2X:%2X:00:11-%02X", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5], idx);
+    sprintf(out, "%02X:%02X:%02X:%02X:%02X:%02X:00:11-%02X", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5], idx);
 
   }
 
